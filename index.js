bb62681f3567cecb71535b1498370a0ac7e85c45
mayrant <mayrant2004@yahoo.com>
2021-07-20 21:46:34 -0500

    installed node and added bodyparser middleware

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 4000
 
+app.use(express.json())
+
 app.get('/', (req, res) => res.send('default route'))
 
 app.listen(port, () => {
