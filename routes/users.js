ed7c99619e83a6f7625453a26faccbab84c62eaa
mayrant <mayrant2004@yahoo.com>
2021-07-29 17:50:24 -0500

    post put and delete routes, put and delete still need review

--- routes/users.js
+++ routes/users.js
@@ -20,4 +20,53 @@ router.get('/users/:id', (req, res) => {
   }
 })
 
+// Create new user
+router.post('/users', (req, res) => {
+  req.body = sampleUser;
+  sampleUser.id = usersData.length + 1;
+
+  usersData.push(sampleUser);
+
+  res.json({ newUser: req.body });
+})
+
+// Update one user
+router.put('/users/:id', (req, res) => {
+  const userFound = usersData.some(user => user.id === parseInt(req.params.id));
+
+  if (userFound) {
+    req.body = sampleUser;
+
+    usersData.forEach(user => {
+      if (user.id === parseInt(req.params.id)) {
+        user.name = sampleUser.name ? sampleUser.name : user.name;
+        user.username = sampleUser.username ? sampleUser.username : user.username; 
+        user.email = sampleUser.email ? sampleUser.email : user.email;
+        user.address = sampleUser.address ? sampleUser.address : user.address;
+        user.phone = sampleUser.phone ? sampleUser.phone : user.phone;
+        user.website = sampleUser.website ? sampleUser.website : user.website;
+        user.company = sampleUser.company ? sampleUser.company : user.company;
+
+        res.json({ msg: 'User updated', user });
+      }
+    }) 
+  } else {
+    res.status(400).json({ msg: `No users found with id ${req.params.id}` })
+  }
+})
+
+// Delete user by id
+router.delete('/users/:id', (req, res) => {
+  const userFound = usersData.some(user => user.id === parseInt(req.params.id));
+
+  if (userFound) {
+    res.json({ 
+      msg: `User ${req.params.id} deleted`, 
+      users: usersData.filter(user => user.id !== parseInt(req.params.id)) 
+    });
+  } else {
+    res.status(400).json({ msg: `No users found with id ${req.params.id}` })
+  }
+})
+
 module.exports = router;
